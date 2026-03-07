41df3a13c0fd9bc492e796290a35de968369a2fa
DeepakGupta7380 <sahudeepak7380.a@gmail.com>
2026-03-07 19:00:42 +0530

    eReact

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+  return(
+  <div>
+    <h1>Hello React</h1>
+    <p>Welcome to React Learning</p>
+    <h2>Deepak Gupta</h2>
+  </div>
+  );
+}
+export default App;
