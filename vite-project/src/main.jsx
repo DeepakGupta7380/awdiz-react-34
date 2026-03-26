34e32568dba917db5ca0df0c82b6aacac7acfc1b
DeepakGupta7380 <sahudeepak7380.a@gmail.com>
2026-03-26 18:35:44 +0530

    usereducer

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -17,6 +17,7 @@ import './index.css'
 // import App3 from './App3.jsx'
 // import ProductCard from './ProductCard.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import UseReducer from './useReducer'
 // import Simple from './Simple.jsx'
 // import Intro from './Intro.jsx'
 // import Recon from './Recon.jsx'
@@ -26,16 +27,17 @@ import { BrowserRouter } from 'react-router-dom'
 // import App4 from './App4.jsx'
 // import App5 from './App5.jsx'
 // import Homes from './Homes.jsx'
-import Use from './App6.jsx'
-import App6 from './App6.jsx'
-import App7 from './App7.jsx'
+// import Use from './App6.jsx'
+// import App6 from './App6.jsx'
+// import App7 from './App7.jsx'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App6 />
+      <UseReducer />
     </BrowserRouter>
 
   </StrictMode>,
